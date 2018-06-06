c9b828552520387e283908d07b8633908c5bbf8d
Naoto Inoue <inoue@hal.t.u-tokyo.ac.jp>
2018-06-06 19:14:10 +0900

    evaluation on both train/test data

---
+++ evaluation.py
@@ -0,0 +1,21 @@
+import torch
+from torchvision.utils import make_grid
+from torchvision.utils import save_image
+
+from util.image import unnormalize
+
+
+def evaluate(model, dataset, device, filename):
+    image, mask, gt = zip(*[dataset[i] for i in range(8)])
+    image = torch.stack(image)
+    mask = torch.stack(mask)
+    gt = torch.stack(gt)
+    with torch.no_grad():
+        output, _ = model(image.to(device), mask.to(device))
+    output = output.to(torch.device('cpu'))
+    output_comp = mask * image + (1 - mask) * output
+
+    grid = make_grid(
+        torch.cat((unnormalize(image), mask, unnormalize(output),
+                   unnormalize(output_comp), unnormalize(gt)), dim=0))
+    save_image(grid, filename)
--- train.py
+++ train.py
@@ -5,16 +5,14 @@ import torch
 from tensorboardX import SummaryWriter
 from torch.utils import data
 from torchvision import transforms
-from torchvision.utils import make_grid
-from torchvision.utils import save_image
 from tqdm import tqdm
 
 import opt
 from dataset import Dataset
+from evaluation import evaluate
 from loss import InpaintingLoss
 from net import PConvUNet
 from net import VGG16FeatureExtractor
-from util.image import unnormalize
 from util.io import load_ckpt
 from util.io import save_ckpt
 
@@ -127,18 +125,9 @@ for i in tqdm(range(start_iter, args.max_iter)):
 
     if (i + 1) % args.vis_interval == 0:
         model.eval()
-        image, mask, gt = zip(*[dataset_val[i] for i in range(8)])
-        image = torch.stack(image)
-        mask = torch.stack(mask)
-        gt = torch.stack(gt)
-        with torch.no_grad():
-            output, _ = model(image.to(device), mask.to(device))
-        output = output.to(torch.device('cpu'))
-        output_comp = mask * image + (1 - mask) * output
-
-        grid = make_grid(
-            torch.cat((unnormalize(image), mask, unnormalize(output),
-                       unnormalize(output_comp), unnormalize(gt)), dim=0))
-        save_image(grid, '{:s}/images/{:d}.jpg'.format(args.save_dir, i + 1))
+        evaluate(model, dataset_train, device,
+                 '{:s}/images/train_{:d}.jpg'.format(args.save_dir, i + 1))
+        evaluate(model, dataset_val, device,
+                 '{:s}/images/test_{:d}.jpg'.format(args.save_dir, i + 1))
 
 writer.close()
